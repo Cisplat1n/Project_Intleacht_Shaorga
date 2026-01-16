a09aef5369dc232f81feb135b8c83656b842946a
Luke S (Desktop) <lukesalvidge@proton.me>
2026-01-16 22:46:25 +0000

    Ver 0.1 Added the initial reading function for data to py and ipynb files

--- llm_data_checker.py
+++ llm_data_checker.py
@@ -1,7 +1,12 @@
 #Libraries
-
+import pandas as pd
 
 #Data Read in function 
+
+def read_df(path):
+    dataframe = pd.read_csv(path)
+    return dataframe
+
 
 
 #Data Processing Prompt + Constraints
