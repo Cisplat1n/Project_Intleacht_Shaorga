1e79c67c524e8a2f37ee309de52f8cde2c762f4c
Luke S (Desktop) <lukesalvidge@proton.me>
2026-01-21 17:14:46 +0000

    Ver 0.1.2 Added datasets for testing and tidied checker script

--- llm_data_checker.py
+++ llm_data_checker.py
@@ -1,18 +1,38 @@
 #Libraries
 import pandas as pd
 
-#Data Read in function 
 
+########################
+#Data Read in function 
+########################
 def read_df(path):
     dataframe = pd.read_csv(path)
     return dataframe
 
 
+########################
+#df checker 
+#(should check for empty data frames and do some basic data Anonymisation)
+########################
 
+
+
+
+
+########################
 #Data Processing Prompt + Constraints
+#(you can build registries in here that help guide the LLM)
+########################
 
 
+
+
+########################
 #Prompt builder
+#(this is where the registries and the prompt will be created)
+########################
 
 
+########################
 #API Client call
+########################
